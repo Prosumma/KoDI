cac278e379b1fc03511eeba03a65e3f85b432b1c
Gregory Higley <greg@prosumma.com>
2021-09-07 00:58:37 -0400

    Simpler boolean test for AbstractAssembly::equals

--- lib/src/main/kotlin/com/prosumma/di/AbstractAssembly.kt
+++ lib/src/main/kotlin/com/prosumma/di/AbstractAssembly.kt
@@ -10,8 +10,7 @@ abstract class AbstractAssembly: Assembly {
     override fun equals(other: Any?): Boolean {
         if (other == null) return false
         if (this === other) return true
-        if (this::class == other::class) return true
-        return false
+        return this::class == other::class
     }
 
     override fun hashCode(): Int = this::class.hashCode()
