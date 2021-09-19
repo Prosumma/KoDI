d3e237a5136c7f96a9b297a3404940520069c224
Gregory Higley <greg@prosumma.com>
2021-09-18 21:45:14 -0400

    Maven

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -42,3 +42,5 @@ dependencies {
     // This dependency is exported to consumers, that is to say found on their compile classpath.
     api("org.apache.commons:commons-math3:3.6.1")
 }
+
+group = "com.github.prosumma"
